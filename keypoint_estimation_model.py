5e4f3d025ab284a6ad291c2313fd38fdcec6b971
plagiagia <dimitris.giannoul@gmail.com>
2020-09-15 11:05:28 +0200

    Dataset Class

--- keypoint_estimation_model.py
+++ keypoint_estimation_model.py
@@ -1,63 +1,28 @@
-# =============================================================================
-# IMPORTS
-# =============================================================================
 import json
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torchvision.transforms as transforms
+from PIL import Image
 from scipy.ndimage import gaussian_filter
 from torch.utils.data import Dataset
 
+
 # =============================================================================
-# DIRECTORIES
+# DATASET CLASS
 # =============================================================================
-# Train
-annotations_dir_train = './annotations/person_keypoints_train2017.json'
-images_dir_train = './train2017/'
-
-# Validation
-annotations_dir_val = './annotations/person_keypoints_val2017.json'
-images_dir_val = './val2017/'
-
-
 class AnnotationsDataset(Dataset):
 
-    def __init__(self, path):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.path = path
-        self.data = self._read_json(self.path)
-        self.keypoints_names = self.data['categories'][0]['keypoints']
-        self.dict_images = self.data['images']
-        self.image_ids = [img['id'] for img in self.data['annotations'] if
-                          img['iscrowd'] == 0 and
-                          img['num_keypoints'] > 0 and
-                          img['bbox'][2] > 100 and
-                          img['bbox'][3] > 100]
+    def __init__(self, annotations_path, img_dir):
+        self.annotations_path = annotations_path
+        self.img_dir = img_dir
+        self.data = self.read_json(self.annotations_path)
+        self.annotations = self.data['annotations']
+        self.keypoint_names = self.data['categories'][0]['keypoints']
+        # Clean annotations
+        self.clean_annotations = self.clean_data(data=self.data)
 
-        self.box = []
-        for each in self.image_ids:
-            self.box.append(self._take_box(each, self.data))
-
-        self.keypoints = []
-        for each in self.image_ids:
-            self.keypoints.append(self._take_keypoints(each, self.data))
-
-        self.images = []
-        for each in self.image_ids:
-            self.images.append(self._load_images(each, self.path))
-
-        self.transformed_images = []
-        for each in self.images:
-            self.transformed_images.append(self._transforms(each))
-
-        self.heatmaps = []
-        for each in zip(self.images, self.keypoints):
-            img, kp = each[0], each[1]
-            self.heatmaps.append(self._heatmaps(img, kp))
-
-    def _read_json(self, path):
+    def read_json(self, path):
         """
         Reads a json file and returns the data
 
@@ -71,90 +36,107 @@ class AnnotationsDataset(Dataset):
             data = json.load(f)
         return data
 
-    def _load_images(self, id, path):
-        for each in self.dict_images:
-            if id == each['id']:
-                image_name = each['file_name']
-        img = plt.imread(path + image_name)
-        return img
-
-    def _img_annotations(self, id, data):
+    def clean_data(self, data):
         """
-        Given the name of an image return the image's annotations
+        Clean the data from images with small boundary boxes or with no annotations
 
         Parameters:
-            id (int): image id
-            data (dict): dataset from which we will read the annotations for each image
-
-         Returns:
-               annotations (dict): A dictionary with all the annotations for each image id
-        """
-        annotations = {}
-        for each in data['annotations']:
-            if id == each['id']:
-                annotations = each
-        return annotations
-
-    def _take_box(self, id, data):
-        """
-        Given the id of an image return the image's box coordinates
-
-        Parameters:
-            id (int): image id
-            data (dict): dataset from which we will read the annotations for each image
-        Returns:
-            box (List): A list with the coordinates of the box [X, y, width, height]
-        """
-        annotations = self._img_annotations(id, data)
-        box = annotations.get('bbox')
-        return box
-
-    def _take_keypoints(self, id, data):
-        """
-        Given the id of an image return the image's keypoints
-
-        Parameters:
-            id (int): image id
-            data (dict): dataset from which we will read the annotations for each image
+            data  : original annotation data
 
         Returns:
-              keypoints (List): A list of lists, of totally 17 keypoints each. [X_keypoints, Y_keypoints, alpha_keypoints]
+            clean_annotations : cleaned annotations
         """
-        annotations = self._img_annotations(id, data)
-        img_keypoints_x = []
-        img_keypoints_y = []
-        img_keypoints_v = []
-        i_old = 0
-        for i in range(3, 54, 3):
-            x, y, v = annotations['keypoints'][i_old:i]
-            img_keypoints_x.append(x)
-            img_keypoints_y.append(y)
-            img_keypoints_v.append(v)
-            i_old = i
-        return [img_keypoints_x, img_keypoints_y, img_keypoints_v]
+        clean_annotations = []
+        for each_annot in data['annotations']:
+            # Checks if the picture shows more than one person
+            if each_annot['iscrowd'] != 0:
+                continue
+            # Checks if the picture has annotations
+            if each_annot['num_keypoints'] < 1:
+                continue
 
-    def _transforms(self, img):
-        transform = transforms.Compose([transforms.ToPILImage(),
-                                        transforms.Resize((256, 192)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                             std=[0.229, 0.224, 0.225])])
-        img_array = torch.from_numpy(img / 255.).permute(2, 0, 1).float()
-        return transform(img_array)
+            # Check if the picture has a big enough box
+            if each_annot['bbox'][2] < 48 and each_annot['bbox'][3] < 64:
+                continue
 
-    def _heatmaps(self, img, keypoints):
-        keypoints = np.array(keypoints)
-        keypoints_downsampled = (keypoints[0] * 256 / img.shape[0] // 4,
-                                 keypoints[1] * 192 / img.shape[1] // 4)
-        validity = [1 if each > 0 else 0 for each in keypoints[2]]
-        heatmap = np.zeros((17, 64, 48))
+            clean_annotations.append(each_annot)
+
+        return clean_annotations
+
+    def load_image(self, annotation):
+        """
+        Loads an image, apply transforms (rescale, resize, normalize, transformed to tensor)
+
+        Parameters:
+            annotation  : Image's annotations
+
+        Returns:
+            tensor_image : Image in tensor form, normalized, resized(192 x 256), rescaled(0...1)
+        """
+        img_name = '000000000000'
+        img_name = img_name[0:len(img_name) - len(str(annotation['image_id']))] + str(annotation['image_id']) + '.jpg'
+        pil_image = Image.open(self.img_dir + '/' + img_name)
+
+        x, y, w, h = annotation['bbox']
+        resized_img = pil_image.resize((192, 256), box=(x, y, x + w, y + h))
+
+        array = np.array(resized_img)
+
+        # Add on more dimension in case image is Black & White
+        if len(array.shape) != 3:
+            array = np.stack((array,) * 3, axis=-1)
+        #     array = np.atleast_3d(array)
+
+        transformations = transforms.Compose([transforms.ToTensor(),
+                                              transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                   std=[0.229, 0.224, 0.225])])
+        rescaled_img = array / 255.
+        tensor_image = transformations(rescaled_img)
+
+        return tensor_image
+
+    def heatmaps(self, annotation):
+        """
+        Creates heatmaps for an image
+
+        Parameters:
+            annotation  : Image's annotations
+
+        Returns:
+            heatmaps_tensor : Heatmaps in tensor form, shape(17, 64, 48)
+            validity_tensor : Tensor with all valid keypoints, shape (17,)
+        """
+
+        keypoints = np.array(annotation['keypoints']).reshape(17, 3)
+        x, y, w, h = annotation['bbox']
+        heatmap_w = 48
+        heatmap_h = 64
+        rescaled_keypoints = np.ceil(
+            (keypoints - [x, y, 0]) * (np.array([heatmap_w, heatmap_h, 1]) / np.array([w, h, 1]))).astype(np.int)
+
+        heatmaps = np.zeros((17, 64, 48))
         for i in range(17):
-            heatmap[i, int(keypoints_downsampled[0][i]), int(keypoints_downsampled[1][i])] = 1
-        heatmap = gaussian_filter(heatmap, sigma=2)
-        for i in range(17):
-            temp = heatmap[i, :, :]
-            heatmap[i, :, :] = ((temp - temp.min()) / (temp.max() - temp.min()))
-        return heatmap
+            if rescaled_keypoints[i][2] > 0:
+                temp_x = rescaled_keypoints[i][1]
+                temp_y = rescaled_keypoints[i][0]
+                heatmaps[i, temp_x, temp_y] = 1.0
+                heatmaps[i, :, :] = gaussian_filter(heatmaps[i, :, :], sigma=2, mode='constant', cval=0.0)
+                heatmaps[i, :, :] = (heatmaps[i, :, :] - np.min(heatmaps[i, :, :])) / (
+                        np.max(heatmaps[i, :, :]) - np.min(heatmaps[i, :, :]))
 
+        validity = [1 if v > 0 else 0 for v in rescaled_keypoints[:, 2]]
 
-test = AnnotationsDataset(annotations_dir_train)
+        validity_tensor = torch.tensor(validity).float()
+        heatmaps_tensor = torch.tensor(heatmaps).float()
+
+        return heatmaps_tensor, validity_tensor
+
+    def __len__(self):
+        return len(self.clean_annotations)
+
+    def __getitem__(self, id):
+        annotation = self.data[id]
+        image = self.load_image(annotation)
+        heatmap, validity = self.heatmaps(annotation)
+
+        return {'image': image, 'heatmap': heatmap, 'validity': validity}
